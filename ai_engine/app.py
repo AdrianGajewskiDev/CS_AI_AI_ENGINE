373e701319e7750f15384098fe5e79bcd83bb6d9
Adrian Gajewski <adrian.gajewski001@gmail.com>
2024-10-11 09:02:09 +0200

    added handler for batch

--- ai_engine/app.py
+++ ai_engine/app.py
@@ -1,5 +1,18 @@
+import sys
 from ai_engine.engine.engine import startup_engine
 
 
 def handler(event: dict, context):
     return startup_engine(event)
+
+def batch_handler(task_id: str): 
+    return startup_engine({'Records': [{'Sns': {'Message': task_id}}]})
+
+if __name__ == '__main__':
+    args = sys.argv
+
+    if len(args) == 2:
+        task_id = args[2]
+        batch_handler(task_id)
+    
+    raise Exception('Invalid arguments')
